c80d26b6529cbf10b29d62266e31fc009212918e
Manman <13760053152@163.com>
2024-06-11 00:05:43 +0800

    TimeOut TimeOutAndInterval

---
+++ JavaScript/JavaScript高级程序设计第四版/chap12/TimeOut.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+  console.log("这是timeout1");
+}, 3000);
+setTimeout(() => {
+  console.log("这是timeout2");
+}, 5000);
+setTimeout(() => {
+  console.log("这是timeout3");
+}, 1000);
+setTimeout(() => {
+  console.log("这是timeout4");
+}, 1000);
